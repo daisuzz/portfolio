5b5078c5af654a27d3e3f551e4bf618aacd75610
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-09-30 21:12:57 +0900

    add experience

--- src/components/DAbout.vue
+++ src/components/DAbout.vue
@@ -2,7 +2,7 @@
   <v-content class="mx-10 py-10">
     <h1>About</h1>
     <p>
-      I'm a software engineer working at Tokyo, Japan. I'm interested in backend development and network security.<br/>
+      I'm a software engineer working at Tokyo, Japan. I'm interested in backend development, network security and agile development.<br/>
       I like coding, reading a book, watching a Rugby, playing a video game, eating Sushi/Yakiniku.
     </p>
   </v-content>
