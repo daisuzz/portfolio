710fa37c8a99f95c0aeb569970d125bb5614311e
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-10-01 21:17:06 +0900

    Aboutを更新

--- src/components/DAbout.vue
+++ src/components/DAbout.vue
@@ -2,8 +2,8 @@
   <v-content class="mx-10 py-10">
     <h1 class="font-italic font-weight-medium">About</h1>
     <p>
-      I'm a software engineer working at Tokyo, Japan. I'm interested in backend development, network security and agile development.<br/>
-      I like coding, reading a book, watching a Rugby, playing a video game, eating Sushi/Yakiniku.
+      I'm a software engineer working at Tokyo, Japan. I'm interested in backend web development, design pattern, architecture, and agile development.<br/>
+      I like coding, reading a book, watching a Rugby, signing a song, playing a video game and eating Sushi/Yakiniku.
     </p>
   </v-content>
 </template>
