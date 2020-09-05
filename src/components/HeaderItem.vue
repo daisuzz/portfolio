df2c218d538007cba0500d6338b66feffee34912
daisuzz <daisaku.rugby@gmail.com>
2020-09-05 15:38:16 +0900

    fix filename

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -11,28 +11,36 @@
       <v-btn icon href="https://twitter.com/daisuzz" target="_blank">
         <v-icon x-large>{{ twitterSvgPath }}</v-icon>
       </v-btn>
-      <v-btn icon href="https://www.linkedin.com/in/daisaku-suzuki-1a37aa161/" target="_blank">
+      <v-btn
+        icon
+        href="https://www.linkedin.com/in/daisaku-suzuki-1a37aa161/"
+        target="_blank"
+      >
         <v-icon x-large>{{ linkedinSvgPath }}</v-icon>
       </v-btn>
       <v-btn icon href="https://iikanji.hatenablog.jp" target="_blank">
         <v-icon x-large>{{ textSvgPath }}</v-icon>
       </v-btn>
     </v-app-bar>
-
   </div>
 </template>
 <script>
-    import {mdiGithubCircle, mdiLinkedinBox, mdiTagTextOutline, mdiTwitterCircle} from '@mdi/js';
+import {
+  mdiGithubCircle,
+  mdiLinkedinBox,
+  mdiTagTextOutline,
+  mdiTwitterCircle
+} from "@mdi/js";
 
-    export default {
-        name: "DHeader",
-        data: () => ({
-            twitterSvgPath: mdiTwitterCircle,
-            gitHubSvgPath: mdiGithubCircle,
-            linkedinSvgPath: mdiLinkedinBox,
-            textSvgPath: mdiTagTextOutline
-        }),
-    };
+export default {
+  name: "DHeader",
+  data: () => ({
+    twitterSvgPath: mdiTwitterCircle,
+    gitHubSvgPath: mdiGithubCircle,
+    linkedinSvgPath: mdiLinkedinBox,
+    textSvgPath: mdiTagTextOutline
+  })
+};
 </script>
 
 <style scoped></style>
