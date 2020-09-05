27ea3a4ed423cb42552a1effc5464f53e6730eb1
daisuzz <daisaku.rugby@gmail.com>
2020-09-05 15:01:05 +0900

    change layout

--- src/components/DAbout.vue
+++ src/components/DAbout.vue
@@ -1,14 +1,25 @@
 <template>
-  <v-content class="mx-10 py-10">
-    <h1 class="font-italic font-weight-medium">About Me</h1>
-    <p>
-      東京でソフトウェアエンジニアとして働いています。<br />
-      アプリケーションの品質を改善したり、開発生産性を向上させるための取り組みや技術が好きです。<br />
-      Software engineer working at Tokyo, Japan. <br />
-      I'm interested in design pattern, software architecture, and agile
-      development.<br />
-    </p>
-  </v-content>
+  <v-layout row wrap>
+    <v-content class="mx-10 py-10">
+      <v-row justify="space-around">
+        <v-col>
+          <v-avatar class="profile" size="180">
+            <img src="../assets/icon.jpg" alt="daisuzz">
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <h1 class="font-italic font-weight-medium">Suzuki Daisaku</h1>
+          <p>
+            東京でソフトウェアエンジニアとして働いています。<br/>
+            アプリケーションの品質を改善したり、開発生産性を向上させるための取り組みや技術が好きです。<br/>
+            Software engineer working at Tokyo, Japan. <br/>
+            I'm interested in design pattern, software architecture, and agile
+            development.<br/>
+          </p>
+        </v-col>
+      </v-row>
+    </v-content>
+  </v-layout>
 </template>
 
 <script>
