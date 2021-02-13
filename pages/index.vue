de70c1d85c1484a169a107c6866b0e3f23c86d1b
daisuzz <daisaku.rugby@gmail.com>
2021-02-13 20:23:46 +0900

    change design

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,16 @@
 <template>
   <v-app>
-    <HeaderItem />
-    <SubHeaderItem />
-    <v-main>
-      <AboutItem />
-      <v-divider></v-divider>
-      <ExperienceItem />
-      <v-divider></v-divider>
-      <SkillItem />
-      <v-divider></v-divider>
-    </v-main>
-    <FooterItem />
+    <AboutItem/>
   </v-app>
 </template>
 
 <script>
-import HeaderItem from "../components/headerItem";
-import SubHeaderItem from "../components/subHeaderItem";
 import AboutItem from "../components/aboutItem";
-import ExperienceItem from "../components/experienceItem";
-import SkillItem from "../components/skillItem";
-import FooterItem from "../components/footerItem";
 
 export default {
   name: "App",
   components: {
-    HeaderItem: HeaderItem,
-    SubHeaderItem: SubHeaderItem,
     AboutItem: AboutItem,
-    ExperienceItem: ExperienceItem,
-    SkillItem: SkillItem,
-    FooterItem: FooterItem
   },
   mounted() {
     document.title = "daisuzz.dev";
